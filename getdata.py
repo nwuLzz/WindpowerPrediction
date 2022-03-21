37c86eae03fa7443b072b96c588bf7c7c68e9325
nwuLzz <da_liuzz@163.com>
2022-03-21 17:46:11 +0800

    上传超短时预测调参过程记录

--- getdata.py
+++ getdata.py
@@ -7,10 +7,15 @@
 
 import pymysql
 import pandas as pd
+import datetime
+import glob
+
+
+farm_code = '30008'     # 风场编号
 
 
 def db_conn():
-    conn = pymysql.connect(host='192.168.0.36', user='liuzhenzhen', passwd='123456', port=5029, db='db30019_2021', charset='utf8')
+    conn = pymysql.connect(host='192.168.0.36', user='liuzhenzhen', passwd='123456', port=5029, db='db'+farm_code+'_2021', charset='utf8')
     return conn
 
 
@@ -22,100 +27,118 @@ def get_data(wtgs):
         print('连接数据库失败！')
         return
     print(wtgs)
-    for i in range(3, 13):  # 3~12月
+    cur = conn.cursor()     # 生成游标对象
+
+    # 取多个月份的数据
+    sql = "SELECT real_time, grWindSpeed, grWindDirction, grOutdoorTemperature, grAirPressure, " \
+          "grAirDensity, grGridActivePower, giWindTurbineOperationMode, " \
+          "gbTurbinePowerLimited, giWindTurbineYawMode, {} as wtid " \
+          "from t{}_all t " \
+          "where month(real_time) in (10, 11, 12) " \
+          "and DATE_FORMAT(real_time,'%i') % 15 = 0 " \
+          "group by DATE_FORMAT(real_time,'%Y-%m-%d %H:%i');".format(wtgs, wtgs)      # 定义SQL语句，每隔15分钟取一条
+    cur.execute(sql)        # 执行SQL语句
+    data = cur.fetchall()   # 获得数据
+    data = pd.DataFrame(data, columns=['real_time', 'grWindSpeed', 'grWindDirction', 'grOutdoorTemperature',
+                                       'grAirPressure', 'grAirDensity', 'grGridActivePower',
+                                       'giWindTurbineOperationMode', 'gbTurbinePowerLimited', 'giWindTurbineYawMode', 'wtid'])
+    print("读取{}条数据！".format(len(data)))
+    data.to_csv('./data/{}/{}_2021_15.csv'.format(farm_code, wtgs), index=False)   # 如果sql数据量太大，可以分批读取并存储，每次修改此文件名即可
+    cur.close()             # 关闭游标
+
+    '''
+    # 按月读数据
+    for month_id in range(10, 13):     # 按月取
         cur = conn.cursor()     # 生成游标对象
         sql = "SELECT real_time, grWindSpeed, grWindDirction, grOutdoorTemperature, grAirPressure, " \
               "grAirDensity, grGridActivePower, giWindTurbineOperationMode, " \
-              "gbTurbinePowerLimited, giWindTurbineYawMode " \
+              "gbTurbinePowerLimited, giWindTurbineYawMode, {} as wtid " \
               "from t{}_all t " \
               "where month(real_time) = {} " \
               "and DATE_FORMAT(real_time,'%i') % 15 = 0 " \
-              "group by DATE_FORMAT(real_time,'%Y-%m-%d %H:%i');".format(wtgs, i)      # 定义SQL语句，按月读取，每隔几分钟取一条
+              "group by DATE_FORMAT(real_time,'%Y-%m-%d %H:%i');".format(wtgs, wtgs, month_id)      # 定义SQL语句，按月读取，每隔几分钟取一条
         cur.execute(sql)        # 执行SQL语句
         data = cur.fetchall()   # 获得数据
         data = pd.DataFrame(data, columns=['real_time', 'grWindSpeed', 'grWindDirction', 'grOutdoorTemperature',
                                            'grAirPressure', 'grAirDensity', 'grGridActivePower',
-                                           'giWindTurbineOperationMode', 'gbTurbinePowerLimited', 'giWindTurbineYawMode'])
+                                           'giWindTurbineOperationMode', 'gbTurbinePowerLimited', 'giWindTurbineYawMode', 'wtid'])
         print("读取{}条数据！".format(len(data)))
-        data.to_csv('./data/'+wtgs+'_2021_'+'{}'.format(i)+'_15.csv', index=False)   # 如果sql数据量太大，可以分批读取并存储，每次修改此文件名即可
+        data.to_csv('./data/{}/{}_2021_{}_15.csv'.format(farm_code, wtgs, month_id), index=False)   # 如果sql数据量太大，可以分批读取并存储，每次修改此文件名即可
         cur.close()             # 关闭游标
+    '''
     conn.close()            # 关闭数据库连接
     return data
 
 
-def combine_csv(wtgs):
+class CombineData:
     """
-        合并多个csv到1个csv
+        数据整合，风机级合并成风场级
     """
-    import glob
+    def __init__(self):
+        pass
 
-    csv_list = glob.glob('./data/{}*.csv'.format(wtgs))
-    print("机组{}共发现{}个csv文件".format(wtgs, len(csv_list)))
-    if len(csv_list) > 0:
-        df_list = []
-        for filename in sorted(csv_list):
-            df_tmp = pd.read_csv(filename)
-            df_tmp.sort_values(by='real_time', inplace=True)        # 按时间排序
-            df_tmp.drop_duplicates(subset=['real_time'], keep='first', inplace=True)        # 去重
-            df_list.append(df_tmp)
-        full_df = pd.concat(df_list)
-        full_df.to_csv('./data/'+wtgs+'_2021_15.csv', index=False)
+    def combine_v(self, wtgs=''):
+        """
+            纵向合并多个csv到1个csv
+            wtgs：风机编号，决定是否按照风机合并。默认为空，代表合并所有文件。
+        """
+        print(wtgs)
+        csv_list = glob.glob('./data/{}/{}*.csv'.format(farm_code, wtgs))   # wtgs非空时，只合并该台机组的数据
+        print("机组{}共发现{}个csv文件".format(wtgs, len(csv_list)))
 
-        # 以下写法会导致每个文件的表头都保留到最终文件
-        # for i in csv_list:
-        #     fr = open(i, 'rb').read()
-        #     with open('./data/30019001_2021_15.csv', 'ab') as f:
-        #         f.write(fr)
-        #     print(i)
-        print("合并完毕！")
-        return full_df
-    else:
-        print("无文件可合并！")
-        return pd.DataFrame()
+        if wtgs == '':
+            wtgs = farm_code
+        dst_path = './data/{}/{}_2021_15.csv'.format(farm_code, wtgs)      # 保存的目标路径
 
+        if len(csv_list) > 0:
+            df_list = []
+            for filename in sorted(csv_list):
+                df_tmp = pd.read_csv(filename)
+                df_tmp.sort_values(by='real_time', inplace=True)        # 按时间排序
+                df_tmp.drop_duplicates(subset=['real_time'], keep='first', inplace=True)        # 去重
+                # df_tmp['wtid'] = filename[13:21]
+                df_list.append(df_tmp)
+            full_df = pd.concat(df_list)
+            full_df.to_csv(dst_path, index=False)
 
-def combine_csv():
-    """
-        合并多个csv到1个csv
-    """
-    import glob
+            # 以下写法会导致每个文件的表头都保留到最终文件
+            # for i in csv_list:
+            #     fr = open(i, 'rb').read()
+            #     with open('./data/30019001_2021_15.csv', 'ab') as f:
+            #         f.write(fr)
+            #     print(i)
+            print("合并完毕！")
+            return full_df
+        else:
+            print("无文件可合并！")
+            return pd.DataFrame()
 
-    csv_list = glob.glob('./data/*.csv')
-    print("共发现{}个csv文件".format(len(csv_list)))
-    if len(csv_list) > 0:
-        df_list = []
-        for filename in sorted(csv_list):
-            df_tmp = pd.read_csv(filename)
-            df_tmp.sort_values(by='real_time', inplace=True)        # 按时间排序
-            df_tmp.drop_duplicates(subset=['real_time'], keep='first', inplace=True)        # 去重
-            df_tmp['wtid'] = filename[7:15]
-            df_list.append(df_tmp)
-        full_df = pd.concat(df_list)
-        print(full_df.head().append(full_df.tail()))
-        full_df.to_csv('./data/30019_2021_15.csv', index=False)
-
-        print("合并完毕！")
-        return full_df
-    else:
-        print("无文件可合并！")
-        return pd.DataFrame()
+    def combine_h(self):
+        """
+            横向合并多个csv到1个csv，比如多个风机点位纵向合并以增加特征，功率相加作为全场功率
+        """
+        pass
 
 
 def main():
-    # wtgs_list = []
-    # for i in range(1, 8):
-    #     wtgs = str(30019000 + i)
-    #     wtgs_list.append(wtgs)
-    #
-    # # 按机组合并文件
-    # for wtgs in wtgs_list:
-    #     # data = get_data(wtgs)
-    #     data = combine_csv(wtgs)
+    wtgs_list = []
+    for i in range(10, 11):
+        wtgs = farm_code + str(i).zfill(3)    # 风机编号
+        wtgs_list.append(wtgs)
+        data = get_data(wtgs)       # 从数据库获取wtgs的scada数据并保存到本地
 
-    # 合并所有文件
-    data = combine_csv()
+    # # 合并文件
+    # for wtgs in wtgs_list:
+    #     cd = CombineData()
+    #     data = cd.combine_v(wtgs)     # 按机组合并
+    #     data = cd.combine_v()     # 合并所有
+
     return data
 
 
 if __name__ == '__main__':
+    st = datetime.datetime.now()
     main()
+    et = datetime.datetime.now()
+    dur = (et - st).seconds
+    print("\n读scada数据耗时：{}秒".format(dur))
